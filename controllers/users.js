3042383b14e20e09f624cb14e988f8500f7e70dd
Ethan Tan <ethantan@Ethans-MacBook-Pro.local>
2022-05-11 16:25:25 -0700

    Modified Users

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,6 @@
 const { prisma } = require(".prisma/client");
 
 const newUser = (req, res, next) => {
-  const allUsers = await prisma.user.findMany()
 
   res.json({message: "POST new user", users: allUsers});
 };
