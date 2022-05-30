e1bede1795c0fac2efd1761276be0a94cef52d2f
Akshaya Sundaram <32802109+akshaya-sundaram@users.noreply.github.com>
2022-05-30 15:51:18 -0700

    Templates for CRUD Database operations on Admin users

--- controllers/users.js
+++ controllers/users.js
@@ -81,6 +81,158 @@ const loginUser = async (req, res) => {
     res.status(200).json(userExists);
 };
 
+/* Below are generic API templates for different CRUD (CREATE READ UPDATE DELETE) operations on the database */
+
+//Template for reading all admin users in the database
+const getUsers = async (req, res) => {
+    const users = await user.findMany({
+        select: {
+            user_id: true,
+            firstName: true,
+            lastName: true
+        },
+        where: {
+
+        }
+    });
+
+    res.json(users);
+};
+
+//Template for finding users by their admin ID
+const getUserById = async (req, res) => {
+    const users = await user.findUnique({
+        select: {
+            user_id: true,
+            firstName: true,
+            lastName: true,
+            email: true
+        },
+        where: {
+            user_id: parseInt(req.params.id)
+        }
+    });
+    if (!users) {
+        return res.status(400).json({
+            msg: 'User NOT found'
+        });
+    }
+
+
+    res.json(users);
+};
+
+//Template for finding users by their lastname
+const getUserByName = async (req, res) => {
+    const users = await user.findMany({
+        select: {
+            user_id: true,
+            email: true
+        },
+        where: {
+            lastName: req.params.lastName
+
+        }
+    });
+
+    if (!users) {
+        return res.status(400).json({
+            msg: 'User NOT found'
+        });
+    }
+
+    res.json(users);
+};
+
+//Template for updating user name and ID by their email
+const updateUserByEmail = async (req, res) => {
+
+
+    if (req.body.email) {
+        const userExists = await user.findUnique({
+            where: {
+                email: req.body.email
+            },
+            select: {
+                email: true
+            }
+        });
+
+        if(userExists) {
+            return res.status(400).json({
+                msg: 'User with email = ' + req.body.email + ' already exists, choose another email'
+            });
+        }
+    }
+
+    const users = await user.update({
+        where: {
+            email: parseInt(req.params.email)
+        },
+        data: {
+            user_id: req.body.user_id,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+        }
+    });
+
+    if (!users) {
+        return res.status(400).json({
+            msg: 'USER NOT found'
+        });
+    }
+    res.json(users);
+};
+
+//Template for deleting admin users from the database by their email 
+const deleteUserByEmail = async (req, res) => {
+    const userExists = await user.findUnique({
+        where: {
+            email: req.params.email
+        },
+        select: {
+            email: true
+        }
+    });
+
+    if(!userExists) {
+        return res.status(400).json({
+            msg: 'USER NOT found'
+        });
+    }
+
+    const users = await user.delete({
+        where: {
+            email: req.params.email
+        }
+    });
+
+    res.json(users);
+};
+
+//Template for finding user name and ID from email
+const getUserByEmail = async (req, res) => {
+    const users = await user.findUnique({
+        select: {
+            user_id: true,
+            firstName: true,
+            lastName: true,
+            email: true
+        },
+        where: {
+            email: req.params.email
+        }
+    });
+    if (!users) {
+        return res.status(400).json({
+            msg: 'User NOT found'
+        });
+    }
+
+
+    res.json(users);
+};
+
 /* To check valid password
 UserSchema.methods.validPassword = function(password) {
 
@@ -92,4 +244,4 @@ UserSchema.methods.validPassword = function(password) {
 https://www.loginradius.com/blog/engineering/password-hashing-with-nodejs/
 */
 
-module.exports = {newUser, loginUser};
+module.exports = {newUser, loginUser, getUsers, getUserById, getUserByEmail, getUserByName, updateUserByEmail, deleteUserByEmail};
